b6e2fa08d0028a875af2f0de18f72a8a05c669e1
AdriantoHartono <adrianto.hartono@my.jcu.edu.au>
2023-03-20 14:43:31 +0800

    Added new file

--- cp1404practicals/prac_02/password_stars.py
+++
@@ -1,9 +0,0 @@
-
-MINIMUM = 10
-password = input("Enter password: ")
-while password < MINIMUM:
-    print()
-    password = input("Enter password: ")
-
-for i in range(0, len(password)):
-    print("*", end="")
